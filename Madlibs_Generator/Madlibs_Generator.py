10a672c43e637e2b7eecdd8b66bd7ae3d1bf9d9f
Nevathan <nevathan321@gmail.com>
2025-05-09 22:56:14 -0400

    Madlibs Project

---
+++ Madlibs_Generator/Madlibs_Generator.py
@@ -0,0 +1,31 @@
+with open("story.txt", "r") as f:
+    story = f.read()
+
+
+
+words = set()
+start_of_word = -1
+
+target_start = "<"
+target_end = ">"
+
+for i, char in enumerate(story):
+    if char == target_start:
+        start_of_word = i 
+    
+    if char == target_end and start_of_word != -1: 
+        word = story[start_of_word:i + 1] 
+        words.add(word) #since were using a set we dont use append, we used set to make sure we have unqiue elements in word (No Repeats)
+        start_of_word = -1
+
+answers = {}
+
+for word in words: 
+    answer = input("Enter a word for" + word + ": ")
+    answers[word] = answer
+
+    
+for word in words:
+    story = story.replace(word, answers[word])
+
+print(story)
